1e6f16c02c8241595b8d9f99e14841e4d5604e87
jasonasav <jasonasav@gmail.com>
2019-02-14 21:37:27 -0800

    App has full functionality, 0 styling. Can toggle todo lists as complete or not, can add a motivation message to each todo list item. All no purged items load on screen load. Need to update with styling, shorten date string.

--- app/app.js
+++ app/app.js
@@ -6,20 +6,7 @@ interact with localstorage
  */
 
 $(document).ready(function(){
-  // this is where we jquery
-  //var keyData = 'ourKey'; // going to need to make this dynamic?
 
-  // maybe load all of our old todo lists when the page loads
-  //  <script>window.onload = function() {
-  // $( "p" ).prepend( $( "b" ) );
-  //  };</script>
-   //http://api.jquery.com/prepend/ 
-  
-//   window.onload = function() {
-//   $('.display-data-item').prepend('.container-data');
-// };
-
-  
   var kolor = function randomColor(){
   var letters = '0123456789ABCDEF';
   var color = '#';
@@ -48,7 +35,11 @@ $(document).ready(function(){
   };
 
     var loadFunc = function() {
-
+      var existing = localStorage.getItem('savedMotivation');
+      var loadMotivation = $('<div class="display-data-item" data-keyValue="savedMotivation">'+"Past Motivation"+existing+'</div>');
+      if(existing) {
+        $(loadMotivation).appendTo('.motivation-container')
+      }
     for( var i = 0; i < localStorage.length; i++) {
       var loadKeyData = localStorage.key(i);
       var loadValueData = localStorage.getItem(localStorage.key(i));
@@ -56,10 +47,7 @@ $(document).ready(function(){
       createChkBox(loadStickyNote);
       createMotivate(loadStickyNote);
       $(loadStickyNote).appendTo('.container-data');
-
     }
-  // $( ".display-data-item" ).prependTo( $( ".container-data" ) );
-  // console.log(e);
    };
   
  window.onload = loadFunc();
@@ -67,45 +55,57 @@ $(document).ready(function(){
 
   $('.btn-add').on('click', function(e){
     console.log(e);
-    //var keyData = $('.input-key').val();
     var keyData = timeStamp();
     var valueData = $('.input-value').val();
-        /*write to db*/
     localStorage.setItem(keyData, valueData);
-    // read from db
     var displayText = keyData + ' | ' + localStorage.getItem(keyData);
-    // this only displays the last one? might want to switch to html
-    // and append a div
-    // <div class="display-data-item" data-keyValue="keyData">valueData</div>
-    // if you use backticks ` you can use ${templateLiterals}
-    // TODO make this vars make sense across the app
     var stickyNote = $('<div class="display-data-item" data-keyValue="'+ keyData +'">'+keyData+ ' ' +valueData+'</div>');
     createChkBox(stickyNote);
     createMotivate(stickyNote);
     $(stickyNote).appendTo('.container-data');
     $('.input-key').val('');
     $('.input-value').val('');
+    
   });
 
   $('.container-data').on('click', '.Motivate', function(e){
     var motivateMessage = $('.jason').val()
-    //motivateMessage.css({"background-color": kolor});
-
-    console.log(e);
+    var existing = localStorage.getItem('savedMotivation')
+      if(existing){
+        localStorage.setItem('savedMotivation', (existing + '  Motivation: ' + motivateMessage))
+      } else {
+        localStorage.setItem('savedMotivation', motivateMessage);
+      }
+    var theData = e.currentTarget.dataset.keyValue;
+    console.log(theData);
+    var keyData = localStorage.key('.data-keyValue');
     $('<span style="color:'+kolor()+'">'+motivateMessage+'</span>').appendTo(e.currentTarget.parentNode);
     $('.jason').val('');
+
   });
 
  
   $('.container-data').on('click', '.chkBox', function(e){
-    console.log(e.currentTarget.dataset.keyvalue);
+    var target = e.currentTarget.parentNode
     var keyData = e.currentTarget.dataset.keyvalue;
-    e.currentTarget.parentNode.remove( e.currentTarget.parentNode); 
+    $(target).css({ "background-color": 'grey' });
   });
-  // delete all?
+  
+
+  $('.check-box').on('click', function() {
+    $('.display-data-item').css({ "background-color": 'white' });
+
+  });
+
+  $('.btn-delete').on('click', function(){
+   
+
+  });
+  
   $('.btn-clear').click(function(){
     localStorage.clear();
     $('.container-data').text('');
+    $('.motivation-container').text('');
   });
 
 });
